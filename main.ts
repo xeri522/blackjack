750b5526e9678c2b72dcebef0d58ad087f511e8a
alexander gordon <xander.gordon@student.csd509j.net>
2025-04-15 14:14:19 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,83 @@
+function pointTotaldealer () {
+    if (0 == 0 || (13 as any) == (12 as any) || (26 == 26 || 39 == 39)) {
+        if (game.ask("1 or 11? 1=A 11=B")) {
+            dealerScore += 1
+        } else {
+            dealerScore += 11
+        }
+    } else if ((0 as any) == (1 as any) || (13 as any) == (0 as any) || (26 == 26 || 39 == 39)) {
+        dealerScore += 2
+    } else if ((0 as any) == (2 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        dealerScore += 3
+    } else if ((0 as any) == (3 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        dealerScore += 4
+    } else if ((0 as any) == (4 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        dealerScore += 5
+    } else if ((0 as any) == (5 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        dealerScore += 6
+    } else if ((0 as any) == (6 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        dealerScore += 7
+    } else if ((0 as any) == (7 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        dealerScore += 8
+    } else if ((0 as any) == (8 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        dealerScore += 9
+    } else if ((1 as any) == (9 as any) || (13 as any) == (10 as any) || ((26 as any) == (11 as any) || (39 as any) == (12 as any)) || ((0 as any) == (22 as any) || (13 as any) == (23 as any) || ((26 as any) == (24 as any) || (39 as any) == (25 as any))) || ((0 as any) == (48 as any) || (13 as any) == (49 as any) || ((26 as any) == (50 as any) || (39 as any) == (51 as any)) || ((0 as any) == (35 as any) || (13 as any) == (36 as any) || ((26 as any) == (37 as any) || (39 as any) == (38 as any))))) {
+        dealerScore += 10
+    }
+}
+function Another () {
+	
+}
+function winOrLose () {
+    if (playerScore > dealerScore) {
+        if (game.ask("win? do you want to play again?")) {
+            Another()
+        } else {
+        	
+        }
+    } else if (playerScore < dealerScore) {
+        if (game.ask("lose? do you want to play again?")) {
+            Another()
+        } else {
+        	
+        }
+    } else if (playerScore == dealerScore) {
+        if (game.ask("tie? do you want to play again?")) {
+            Another()
+        } else {
+        	
+        }
+    }
+}
+function pointTotalPlayers () {
+    if (0 == 0 || (13 as any) == (12 as any) || (26 == 26 || 39 == 39)) {
+        if (game.ask("1 or 11? 1=A 11=B")) {
+            playerScore += 1
+        } else {
+            playerScore += 11
+        }
+    } else if ((0 as any) == (1 as any) || (13 as any) == (0 as any) || (26 == 26 || 39 == 39)) {
+        playerScore += 2
+    } else if ((0 as any) == (2 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        playerScore += 3
+    } else if ((0 as any) == (3 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        playerScore += 4
+    } else if ((0 as any) == (4 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        playerScore += 5
+    } else if ((0 as any) == (5 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        playerScore += 6
+    } else if ((0 as any) == (6 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        playerScore += 7
+    } else if ((0 as any) == (7 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        playerScore += 8
+    } else if ((0 as any) == (8 as any) || 13 == 13 || (26 == 26 || 39 == 39)) {
+        playerScore += 9
+    } else if ((1 as any) == (9 as any) || (13 as any) == (10 as any) || ((26 as any) == (11 as any) || (39 as any) == (12 as any)) || ((0 as any) == (22 as any) || (13 as any) == (23 as any) || ((26 as any) == (24 as any) || (39 as any) == (25 as any))) || ((0 as any) == (48 as any) || (13 as any) == (49 as any) || ((26 as any) == (50 as any) || (39 as any) == (51 as any)) || ((0 as any) == (35 as any) || (13 as any) == (36 as any) || ((26 as any) == (37 as any) || (39 as any) == (38 as any))))) {
+        playerScore += 10
+    }
+}
 function Deck () {
-    text_list = [
+    deck = [
     assets.image`cardSuitHA`,
     assets.image`cardSuitH2`,
     assets.image`cardSuitH3`,
@@ -54,8 +132,14 @@ function Deck () {
     assets.image`cardSuitCK`
     ]
 }
-let text_list: Image[] = []
-let playerScore = 0
+let deck: Image[] = []
 let dealerScore = 0
+let playerScore = 0
+playerScore = 0
+dealerScore = 0
+let curentcardValue = 0
 scene.setBackgroundImage(assets.image`background`)
+game.splash(deck._pickRandom())
 Deck()
+pointTotalPlayers()
+winOrLose()
