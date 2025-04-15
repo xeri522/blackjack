4e810876567a9d00c76db570c1ffa259de8bc388
alexander gordon <xander.gordon@student.csd509j.net>
2025-04-15 13:13:05 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,64 @@
+function Deck () {
+    text_list = [
+    assets.image`cardSuitHA`,
+    assets.image`cardSuitH2`,
+    assets.image`cardSuitH3`,
+    assets.image`cardSuitH4`,
+    assets.image`cardSuitH5`,
+    assets.image`cardSuitH6`,
+    assets.image`cardSuitH7`,
+    assets.image`cardSuitH8`,
+    assets.image`cardSuitH9`,
+    assets.image`cardSuitH10`,
+    assets.image`cardSuitHU`,
+    assets.image`cardSuitHQ`,
+    assets.image`cardSuitHK`,
+    assets.image`cardSuitDA`,
+    assets.image`cardSuitD2`,
+    assets.image`cardSuitD3`,
+    assets.image`cardSuitD4`,
+    assets.image`cardSuitD5`,
+    assets.image`cardSuitD6`,
+    assets.image`cardSuitD7`,
+    assets.image`cardSuitD8`,
+    assets.image`cardSuitD9`,
+    assets.image`cardSuitD10`,
+    assets.image`cardSuitDU`,
+    assets.image`cardSuitDQ`,
+    assets.image`cardSuitDK`,
+    assets.image`cardSuitSA`,
+    assets.image`cardSuitS2`,
+    assets.image`cardSuitS3`,
+    assets.image`cardSuitS4`,
+    assets.image`cardSuitS5`,
+    assets.image`cardSuitS6`,
+    assets.image`cardSuitS7`,
+    assets.image`cardSuitS8`,
+    assets.image`cardSuitS9`,
+    assets.image`cardSuitS10`,
+    assets.image`cardSuitSU`,
+    assets.image`cardSuitSQ`,
+    assets.image`cardSuitSK`,
+    assets.image`cardSuitCA`,
+    assets.image`cardSuitC2`,
+    assets.image`cardSuitC3`,
+    assets.image`cardSuitC4`,
+    assets.image`cardSuitC5`,
+    assets.image`cardSuitC6`,
+    assets.image`cardSuitC7`,
+    assets.image`cardSuitC8`,
+    assets.image`cardSuitC9`,
+    assets.image`cardSuitC10`,
+    assets.image`cardSuitCU`,
+    assets.image`cardSuitCQ`,
+    assets.image`cardSuitCK`
+    ]
+}
+let text_list: Image[] = []
+let playerScore = 0
+let dealerScore = 0
+enum DesignTemplates {
+    PlayingCards
+}
+scene.setBackgroundImage(assets.image`background`)
+Deck()
